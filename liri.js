e535ff0ff4c0de0fc792e9b2d2625be46946a1d8
chris284891 <chris284891@gmail.com>
2019-07-27 18:46:48 -0400

    added log.txt and code for bands-in-town

--- liri.js
+++ liri.js
@@ -21,7 +21,44 @@ inquirer.prompt([
     },
 ])
 
-function (result) {
-    let artist = result.atist;
-    let queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-}
+    .then(function (concert) {
+        if (concert.choice === "concert-this") {
+            console.log("\n-----------------------");
+            console.log("\nHello ${concert.username}");
+            console.log("\n------------------------");
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "What band would you like to see?",
+                    name: "artist"
+                }
+            ])
+            .then(function (result) {
+                let artist = result.atist;
+                let queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+                if(result.artist == ""){
+                    console.log("Please enter in a band");
+                } else {
+                    axios.get(queryUrl).then(
+                        function (response) {
+                            for (let i = 0; i < response.data.length; i++) {
+                                let date = moment(respose.data[i].datetime).format("MM/DD/YYYY")
+                                console.log("\n----------------------");
+                                console.log("Venue: ${response.data[i].data[i].venue.name}");
+                                console.log("Date: ${date}");
+                                console.log("-------------------------");
+                            }
+                            fs.appendFile("log.txt", "\nArtist: ${artist}", function(error) {
+                                if (error) {
+                                    console.log(error);
+                                }
+                                else {
+                                    console.log("Artist $(artist.toUpperCase()} appended to log.txt");
+                                }
+                            });
+                        }
+                    )
+                }
+            }
+        }
+    })
