de185293106942da62b15e5f3477d658928e0156
chris284891 <chris284891@gmail.com>
2019-07-27 17:31:29 -0400

    added inquirer, fs, and some prompts

--- liri.js
+++ liri.js
@@ -1,3 +1,27 @@
 require("dotenv").config();
-  var keys = require("./keys.js");
-  var spotify = new Spotify(keys.spotify);
+const keys = require("./keys.js");
+let moment = require("moment")
+moment().format();
+let fs = require("fs");
+let axios = require("axios");
+var Spotify = require("node-spotify-api");
+let spotify = new Spotify(keys.spotify);
+
+inquirer.prompt([
+    {
+        type: "input",
+        message: "Hi, what may I call you",
+        name: "username"
+    },
+    {
+        type: "list",
+        message: "Would you like to look up a band on Spotify? Or look up if said band has any shows coming up? Or maybe you just want to look up a movie?",
+        choices: ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"],
+        name: "choice"
+    },
+])
+
+function (result) {
+    let artist = result.atist;
+    let queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+}
